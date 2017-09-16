6e0991f67c842d54c5db6b036befdef878981671
glenndejaeger <glenn.dejaeger@gmail.com>
2017-09-16 15:58:29 +0200

    Bootstrap, font awesome + utils added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { WebBluetoothModule } from '@manekinekko/angular-web-bluetooth';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WebBluetoothModule.forRoot({
+      enableTracing: true // enable logs
+    }),
+    NgbModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
